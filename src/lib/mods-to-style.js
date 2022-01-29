74087a58eff91f409ce50e4d55219925286edc54
Reaper <ahoy@barelyhuman.dev>
2022-01-29 22:10:53 +0530

    make code modular and add minification

--- src/lib/mods-to-style.js
+++ src/lib/mods-to-style.js
@@ -6,6 +6,90 @@ const justifyRegex =
 const centerRegex = /^center(X|Y)?$/
 const flexDirectionRegex = /^(row|column)(Reverse)?$/
 
+const applyFlex = (prop, o = {}) => {
+	flexRegex.test(prop) && (o.display = 'flex') && true
+}
+
+const applyBasis = (prop, o = {}) => {
+	if (!flexBasisRegex.test(prop)) {
+		return false
+	}
+	const matches = flexBasisRegex.exec(prop)
+	o.flex = matches[1]
+	return true
+}
+
+const applyAlignments = (prop, o = {}) => {
+	if (!alignRegex.test(prop)) {
+		return false
+	}
+	const matches = alignRegex.exec(prop)
+
+	let cleanValue = matches[1].toLowerCase()
+
+	if (['start', 'end'].includes(cleanValue)) {
+		cleanValue = 'flex-' + cleanValue
+	}
+
+	o.alignItems = cleanValue
+	return true
+}
+
+const applyJustifications = (prop, o = {}) => {
+	if (!justifyRegex.test(prop)) {
+		return false
+	}
+	const matches = justifyRegex.exec(prop)
+	let cleanValue = matches[1].toLowerCase()
+
+	if (['start', 'end'].includes(cleanValue)) {
+		cleanValue = 'flex-' + cleanValue
+	}
+
+	if (['between', 'around', 'evenly'].includes(cleanValue)) {
+		cleanValue = 'space-' + cleanValue
+	}
+
+	o.justifyContent = cleanValue
+	return true
+}
+
+const applyDirections = (prop, o = {}) => {
+	if (!flexDirectionRegex.test(prop)) {
+		return false
+	}
+	const matches = flexDirectionRegex.exec(prop)
+	let cleanDirection = matches[1].toLowerCase()
+	let reverse = matches[2] && matches[2].length && matches[2].toLowerCase()
+	if (reverse) {
+		cleanDirection += '-reverse'
+	}
+	o.flexDirection = cleanDirection
+	return true
+}
+
+const applyCenter = (prop, o = {}) => {
+	if (!centerRegex.test(prop)) {
+		return false
+	}
+
+	const matches = centerRegex.exec(prop)
+
+	if (matches[1] === undefined) {
+		o.alignItems = 'center'
+		o.justifyContent = 'center'
+	}
+
+	if (matches[1] === 'X') {
+		o.justifyContent = 'center'
+	}
+
+	if (matches[1] === 'Y') {
+		o.alignItems = 'center'
+	}
+	return true
+}
+
 /**
  * @name modsToStyle
  * @description convert given modifiers object of the form
@@ -21,79 +105,20 @@ export function modsToStyle(mods) {
 	const sanitizedProps = {...mods}
 
 	for (const key of Object.keys(mods)) {
-		let usedKey = false
-		if (flexRegex.test(key)) {
-			usedKey = true
-			style.display = 'flex'
+		if (
+			!(
+				applyFlex(key, style) ||
+				applyBasis(key, style) ||
+				applyAlignments(key, style) ||
+				applyDirections(key, style) ||
+				applyJustifications(key, style) ||
+				applyCenter(key, style)
+			)
+		) {
+			continue
 		}
 
-		if (flexBasisRegex.test(key)) {
-			usedKey = true
-			const matches = flexBasisRegex.exec(key)
-			style.flex = matches[1]
-		}
-
-		if (alignRegex.test(key)) {
-			usedKey = true
-			const matches = alignRegex.exec(key)
-
-			let cleanValue = matches[1].toLowerCase()
-
-			if (['start', 'end'].includes(cleanValue)) {
-				cleanValue = 'flex-' + cleanValue
-			}
-
-			style.alignItems = cleanValue
-		}
-
-		if (flexDirectionRegex.test(key)) {
-			usedKey = true
-			const matches = flexDirectionRegex.exec(key)
-			let cleanDirection = matches[1].toLowerCase()
-			let reverse = matches[2] && matches[2].length && matches[2].toLowerCase()
-			if (reverse) {
-				cleanDirection += '-reverse'
-			}
-			style.flexDirection = cleanDirection
-		}
-
-		if (justifyRegex.test(key)) {
-			usedKey = true
-			const matches = justifyRegex.exec(key)
-			let cleanValue = matches[1].toLowerCase()
-
-			if (['start', 'end'].includes(cleanValue)) {
-				cleanValue = 'flex-' + cleanValue
-			}
-
-			if (['between', 'around', 'evenly'].includes(cleanValue)) {
-				cleanValue = 'space-' + cleanValue
-			}
-
-			style.justifyContent = cleanValue
-		}
-
-		if (centerRegex.test(key)) {
-			usedKey = true
-			const matches = centerRegex.exec(key)
-
-			if (matches[1] === undefined) {
-				style.alignItems = 'center'
-				style.justifyContent = 'center'
-			}
-
-			if (matches[1] === 'X') {
-				style.justifyContent = 'center'
-			}
-
-			if (matches[1] === 'Y') {
-				style.alignItems = 'center'
-			}
-		}
-
-		if (usedKey) {
-			delete sanitizedProps[key]
-		}
+		delete sanitizedProps[key]
 	}
 
 	return {style, sanitizedProps}
